332553d1fcb7a885ced213d58110e4eeb026eadf
sona angdembe <asonaxaviersm1@gmail.com>
2023-08-12 20:44:38 +0545

    now just remains just  to modify for scren

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbwvaXLfojnXolExp95IxLjpd3vHwpetHvcDqFdmKG62UA9EZMGFngSNat4CCiF-3iPKyw/exec'
+        const form = document.forms['submit-to-google-sheet']
+      const msg = document.getElementById('msg')
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = "Message Sucessfully Sent.Thank You"
+                setTimeout(function(){
+                    msg.innerHTML = ""
+                
+            },5000)
+            form.reset()
+        })
+            .catch(error => console.error('Error!', error.message))
+
+        })
+      
